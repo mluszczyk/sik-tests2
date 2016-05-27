2326d5f0b2c890e3d9aa2cceb9b16a741eb8d538
VirrageS <virrages@gmail.com>
2016-05-27 21:51:02 +0200

    Add: player tests; Fix: master tests (timeouts)

--- test_master.py
+++ test_master.py
@@ -1,17 +1,18 @@
 import contextlib
 import unittest
 import subprocess
+import unittest
 
 
 class Master(subprocess.Popen):
     def __init__(self, args=(), *, stdout=subprocess.PIPE, stderr=subprocess.DEVNULL):
-        super().__init__(("../zad2/Debug/master",) + args, stdout=stdout, stderr=stderr)
-
+        super().__init__(("../SK/radio-streaming/master",) + args, stdout=stdout, stderr=stderr)
 
 @contextlib.contextmanager
 def master_context(*args, **kwargs):
     program = Master(*args, **kwargs)
     yield program
+
     program.kill()
     program.wait()
 
@@ -19,34 +20,38 @@ def master_context(*args, **kwargs):
 class TestArguments(unittest.TestCase):
     def test_no_parameters(self):
         with master_context(()) as program:
-            line = program.communicate()[0]
-            self.assertTrue(line)
+            with self.assertRaises(subprocess.TimeoutExpired):
+                line = program.communicate(timeout=1)[1]
 
     def test_one_parameter(self):
-        with master_context(("234",)) as program:
-            line = program.communicate()[0]
-            self.assertIn(b"234", line)
+        with master_context(("50000",)) as program:
+            with self.assertRaises(subprocess.TimeoutExpired):
+                line = program.communicate(timeout=1)[1]
 
     def test_wrong_number(self):
         with master_context(("234", "234"), stdout=subprocess.DEVNULL, stderr=subprocess.PIPE) as program:
-            line = program.communicate()[1]
+            line = program.communicate(timeout=1)[1]
             self.assertTrue(line)
-            self.assertEqual(program.wait(), 1)
+            self.assertEqual(program.wait(timeout=1), 1)
 
     def test_not_a_number_suffix(self):
         with master_context(("234asdf",), stdout=subprocess.DEVNULL, stderr=subprocess.PIPE) as program:
-            line = program.communicate()[1]
+            line = program.communicate(timeout=1)[1]
             self.assertTrue(line)
-            self.assertEqual(program.wait(), 1)
+            self.assertEqual(program.wait(timeout=1), 1)
 
     def test_number_too_long(self):
         with master_context(("12345" * 10,), stdout=subprocess.DEVNULL, stderr=subprocess.PIPE) as program:
-            line = program.communicate()[1]
+            line = program.communicate(timeout=1)[1]
             self.assertTrue(line)
-            self.assertEqual(program.wait(), 1)
+            self.assertEqual(program.wait(timeout=1), 1)
 
     def test_not_a_number_at_all(self):
         with master_context(("ciastka",), stdout=subprocess.DEVNULL, stderr=subprocess.PIPE) as program:
-            line = program.communicate()[1]
+            line = program.communicate(timeout=1)[1]
             self.assertTrue(line)
-            self.assertEqual(program.wait(), 1)
+            self.assertEqual(program.wait(timeout=1), 1)
+
+
+if __name__ == '__main__':
+    unittest.main()
